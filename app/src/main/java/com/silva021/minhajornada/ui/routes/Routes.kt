85b5be6eb4364e1ea663d4db5cc4f1dff70bee62
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-01 14:33:23 -0300

    add navigation routes for app screens

---
+++ app/src/main/java/com/silva021/minhajornada/ui/routes/Routes.kt
@@ -0,0 +1,41 @@
+package com.silva021.minhajornada.ui.routes
+
+import androidx.navigation.NavController
+
+sealed class Routes(val route: String) {
+    fun popBackStack(navController: NavController) {
+        navController.popBackStack()
+    }
+
+    object ProfileScreen : Routes("profile_screen") {
+        fun navigateToProfileScreen(navController: NavController) {
+            navController.navigate(ProfileScreen.route)
+        }
+    }
+
+    object ChallengesScreen : Routes("challenges_screen") {
+        fun navigateToChallengesScreen(navController: NavController) {
+            navController.navigate(ChallengesScreen.route)
+        }
+    }
+
+    object CreateChallengesScreen : Routes("create_challenges_screen") {
+        fun navigateToCreateChallengesScreen(navController: NavController) {
+            navController.navigate(CreateChallengesScreen.route)
+        }
+    }
+
+    object UpdateChallengeProgressScreen : Routes("update_challenge_progress_screen") {
+        fun navigateToUpdateChallengeProgressScreen(navController: NavController) {
+            navController.navigate(UpdateChallengeProgressScreen.route)
+        }
+    }
+
+    object CommunitiesScreen : Routes("communities_screen") {
+        fun navigateToCommunitiesScreen(navController: NavController) {
+            navController.navigate(CommunitiesScreen.route)
+        }
+    }
+
+    object WelcomeScreen : Routes("welcome_screen")
+}
