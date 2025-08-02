9877249b26d10fe54bd4fa8ca8fabdc485af142c
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-02 16:26:44 -0300

    refactor DatabaseFake to include sample comments and posts for enhanced testing

--- app/src/main/java/com/silva021/minhajornada/ui/DatabaseFake.kt
+++ app/src/main/java/com/silva021/minhajornada/ui/DatabaseFake.kt
@@ -1,6 +1,8 @@
 package com.silva021.minhajornada.ui
 
 import com.silva021.minhajornada.domain.model.CategoryType
+import com.silva021.minhajornada.domain.model.Comment
+import com.silva021.minhajornada.domain.model.Post
 import com.silva021.minhajornada.domain.model.PublicChallenge
 
 object DatabaseFake {
@@ -75,6 +77,49 @@ object DatabaseFake {
             participantsCount = 512,
             category = CategoryType.EDUCATION,
             isTrending = true
+        )
+    )
+
+    val comments = listOf(
+        Comment(
+            userImage = "https://lh3.googleusercontent.com/aida-public/AB6AXuAZlM4wvIsS3fmlcpSyTvhzzjZF7Q_Op85f3s0ny4TpDiU-MIz49SBtb9Gf8hUhkr2iIUVhMgywH7u7NsQDYdwmZE6Ss76FL1wjeaN_JFtmde7n6Tvw1KG87xs1qXLva771J63ZtojlMI36UezCJsXxCWUtPwLSS99k5PTQHq0TPMHcjrIoyG-em8wHtkwKnhrbJDoxHXm6kTMndU3tTbFbhOIu-e4JsCDfBa5hkydz3oja5kRYelhhzV28TBYRJgWvlqFXyo_pMuI",
+            userName = "Ethan Carter",
+            timeAgo = "1d",
+            comment = "Isso é incrível, Jessica! Eu sempre quis aprender espanhol também. Me avise se precisar de um parceiro para praticar!"
+        ),
+        Comment(
+            userImage = "https://lh3.googleusercontent.com/aida-public/AB6AXuCdBDfNXrteRJRjT-vER_7GASE5paihBZU6VF70j2REcHW5gn-lwFiYXIM82It99QIMKjnV8lgTWC5KzqQiA1o6S_Gfan0pgb5JnJ9Jnf2JPLn07FHfi13bmRApe7B6w3WEvJuQJRwzupIFK2iNI0nlmcuv6tNBoPMdO8O6i1rblXKZg4gHzSCnkHIFagBK2b0nHwqofhGfaCZGrQqOkUKFaYLGGvFDRt8htR1eaUIwA_QkEZhucRg9nFNL_kxX7xyUFU6RkyWAuU0",
+            userName = "Olivia Davis",
+            timeAgo = "2d",
+            comment = "Boa sorte! Tenho certeza que você vai se sair bem. Lembre-se de celebrar seu progresso ao longo do caminho."
+        )
+    )
+
+    val postList = listOf(
+        Post(
+            userImage = "https://lh3.googleusercontent.com/aida-public/AB6AXuDtOZOGqPBmoMIxOJODpUF6L55FFzYzDm-7hdl2NAIn44H2ViOQAGcW_9BLQ1JXgaCtWCA4Faq3I-uU4MF6pDd9ixBDalXweY9q1Tz1CBZe5F0dCHySwJjn21fBCZ-dHBU_lDUbFSItE21a0vIT-wO9bdYw1hPv5ej6XyBzb5sVHP3Lh0hi59j48AntkhtNH0ADIm--kflWd8AZa-ZA7GmULHesWk8EcM98tMB3ebeLOk5LiaU9-dgp-qwq5ouQj3JP4v1mc73v1L8",
+            userName = "Sarah Miller",
+            timeAgo = "2d",
+            content = "Acabei de completar minha corrida de 5km! Me sentindo ótima e cheia de energia para o dia. Quem mais está alcançando seus objetivos fitness hoje?",
+            likes = 23,
+            comments = 5
+        ),
+
+        Post(
+            userImage = "https://lh3.googleusercontent.com/aida-public/AB6AXuD38031a9CUlzl_YS565weCOqjozwjPs_D8QhTpnQNCNbreXCZXtJmMNmIYPu0l8vmmXMGS3EMcPwJG-tlMUr_-Y0y3qxdtsG0HyurbO0MBouSoZDdD9dX7QgSFqDi5nyeq3Hrioaln2cG345utbSVfOeEuzmpREIlRnlAJXNB_VTsajQcFh-KuG4hX_vWA1CaeRpIXU-1j-ZH20kzY2vxMbAvV1mpjvFXMgD82P80FTne_XTXi1yd1pVPBpE7GXZ-zHFKHnG1yHqo",
+            userName = "Mark Thompson",
+            timeAgo = "1sem",
+            content = "Estou tentando incluir mais treino de força na minha rotina. Alguma dica para iniciantes?",
+            likes = 15,
+            comments = 3
+        ),
+        Post(
+            userImage = "https://lh3.googleusercontent.com/aida-public/AB6AXuCbc0xLKqOdwb-Vi-QOdbZm9ShGVL6V1BcoLgrMCJ3aRNaobT2QFOCFnWF0fp1EdXFif8_exZfOcq5ANfbEtGZ0p1g9KqcNdtOvcbY_uq7jj9I4xYcnoZ4TZr5SEyfXs8ZdKwJ7bWwBwORGdm82ELnptKa1VcaLPNxIuSVLa5FQj6DGoPeTlEPTmiGbswtKYmBST5fc7gxGBIJB4rN1miXSXWb1BJfCvVrJEQtSzC6eoQAJzLD5A9yAUeUeuDEKQUo-oht2W1yKdQ8",
+            userName = "Emily Carter",
+            timeAgo = "3sem",
+            content = "Alguém animado para uma trilha no final de semana? Vamos explorar novas trilhas e aproveitar o ar livre juntos!",
+            likes = 30,
+            comments = 8
         )
     )
 }
