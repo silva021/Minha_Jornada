7dc8e0b95d372040931205dc11efe4d8c90cb625
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-03 02:57:12 -0300

    refactor: update GetChallengesUseCase to separate active and completed challenges

--- app/src/main/java/com/silva021/minhajornada/domain/usecases/GetChallengesUseCase.kt
+++ app/src/main/java/com/silva021/minhajornada/domain/usecases/GetChallengesUseCase.kt
@@ -1,6 +1,7 @@
 package com.silva021.minhajornada.domain.usecases
 
 import com.silva021.minhajornada.data.repository.ChallengeRepository
+import com.silva021.minhajornada.domain.extension.isCompleted
 import com.silva021.minhajornada.domain.model.Challenges
 import com.silva021.minhajornada.domain.model.toDomain
 
@@ -8,6 +9,10 @@ class GetChallengesUseCase(
     val repository: ChallengeRepository,
 ) {
     suspend operator fun invoke(): Result<Challenges> = runCatching {
-        repository.getChallenges().toDomain()
+        val challenges = repository.getChallenges().map { it.toDomain() }
+        Challenges(
+            actives = challenges.filter { !it.isCompleted() },
+            completed = challenges.filter { it.isCompleted() }
+        )
     }
 }
