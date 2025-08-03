83314c6ba02bf68539355609ce9fe6db2c3c422b
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-03 02:57:26 -0300

    refactor: update GetCommunitiesUseCase to convert repository data to domain model

--- app/src/main/java/com/silva021/minhajornada/domain/usecases/GetCommunitiesUseCase.kt
+++ app/src/main/java/com/silva021/minhajornada/domain/usecases/GetCommunitiesUseCase.kt
@@ -2,11 +2,12 @@ package com.silva021.minhajornada.domain.usecases
 
 import com.silva021.minhajornada.data.repository.CommunitiesRepository
 import com.silva021.minhajornada.domain.model.Communities
+import com.silva021.minhajornada.domain.model.toDomain
 
 class GetCommunitiesUseCase(
     private val repository: CommunitiesRepository
 ) {
     suspend operator fun invoke(): Result<Communities> = runCatching {
-        repository.getCommunities()
+        repository.getCommunities().toDomain()
     }
 }
