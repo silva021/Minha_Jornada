0fed3d6c8ec03f435516eb0ba3b23454ea7e51dd
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-03 02:56:58 -0300

    refactor: update CreateChallengesScreen to improve challenge creation UI and functionality

--- app/src/main/java/com/silva021/minhajornada/ui/screens/challenges/create/CreateChallengesScreen.kt
+++ app/src/main/java/com/silva021/minhajornada/ui/screens/challenges/create/CreateChallengesScreen.kt
@@ -11,18 +11,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,7 +30,11 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.silva021.minhajornada.domain.model.CategoryType
+import com.silva021.minhajornada.domain.model.DurationType
+import com.silva021.minhajornada.ui.components.CategoriesFilter
 import com.silva021.minhajornada.ui.components.CustomTextField
+import com.silva021.minhajornada.ui.components.DurationFilter
 import com.silva021.minhajornada.ui.components.Header
 import com.silva021.minhajornada.ui.components.PrimaryButton
 import com.silva021.minhajornada.ui.theme.Palette
@@ -45,13 +47,13 @@ import com.silva021.minhajornada.ui.theme.Palette.textSecondary
 @Composable
 fun CreateChallengesScreen(
     onBackPressed: () -> Unit,
-    ) {
-
-    val templateName = remember { mutableStateOf("") }
+) {
+    val title = remember { mutableStateOf("") }
     val description = remember { mutableStateOf("") }
     val duration = remember { mutableStateOf("") }
-    val isPublic = remember { mutableStateOf(false) }
     val notificationsEnabled = remember { mutableStateOf(false) }
+    var selectedCategory by remember { mutableStateOf(CategoryType.FITNESS) }
+    var selectedDuration by remember { mutableStateOf(DurationType.THREE_DAYS) }
 
     Column(
         modifier = Modifier
@@ -64,18 +66,17 @@ fun CreateChallengesScreen(
                 .background(Palette.backgroundColor)
                 .padding(horizontal = 16.dp)
         ) {
-            Spacer(modifier = Modifier.height(16.dp))
 
             Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
                 Text(
-                    text = "Nome do Modelo",
+                    text = "Nome do Desafio",
                     color = textSecondary,
                     fontWeight = FontWeight.Medium
                 )
 
                 CustomTextField(
-                    value = templateName.value,
-                    onValueChange = { templateName.value = it },
+                    value = title.value,
+                    onValueChange = { title.value = it },
                     placeholder = "Ex: Desafio Fitness de 30 Dias",
                     modifier = Modifier.fillMaxWidth(),
                 )
@@ -102,23 +103,39 @@ fun CreateChallengesScreen(
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-                Text(
-                    text = "Duração",
-                    color = textSecondary,
-                    fontWeight = FontWeight.Medium
-                )
+            Text(
+                text = "Duração",
+                color = textPrimary,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
+            )
 
-                CustomTextField(
-                    value = duration.value,
-                    onValueChange = { duration.value = it },
-                    placeholder = "Ex: 30 dias",
-                    modifier = Modifier.fillMaxWidth(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
-                )
+            DurationFilter(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                selectedDuration = selectedDuration
+            ) {
+                selectedDuration = it
             }
 
-            Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Categorias",
+                color = textPrimary,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            CategoriesFilter(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                selectedCategory = selectedCategory
+            ) {
+                selectedCategory = it
+            }
 
             Text(
                 text = "Configurações",
@@ -127,7 +144,7 @@ fun CreateChallengesScreen(
                 fontWeight = FontWeight.SemiBold
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
             Column(
                 modifier = Modifier
@@ -135,39 +152,6 @@ fun CreateChallengesScreen(
                     .clip(RoundedCornerShape(12.dp))
                     .background(Palette.cardBackground)
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Column {
-                        Text(
-                            text = "Visibilidade",
-                            color = textPrimary,
-                            fontWeight = FontWeight.Medium
-                        )
-                        Text(
-                            text = if (isPublic.value) "Público" else "Privado",
-                            color = textSecondary,
-                            fontSize = 14.sp
-                        )
-                    }
-                    Switch(
-                        checked = isPublic.value,
-                        onCheckedChange = { isPublic.value = it },
-                        colors = SwitchDefaults.colors(
-                            checkedThumbColor = Color.White,
-                            checkedTrackColor = Palette.primaryColor,
-                            uncheckedThumbColor = Color.White,
-                            uncheckedTrackColor = accentColor
-                        )
-                    )
-                }
-
-                HorizontalDivider(thickness = 1.dp, color = accentColor)
-
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -202,44 +186,15 @@ fun CreateChallengesScreen(
 
             Spacer(modifier = Modifier.weight(1f))
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Palette.backgroundColor)
-                    .padding(16.dp)
-            ) {
-                PrimaryButton(
-                    onClick = { /* Salvar alterações */ },
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "Salvar Alterações"
-                )
+            PrimaryButton(
+                onClick = { /* Salvar alterações */ },
+                modifier = Modifier.fillMaxWidth(),
+                text = "Salvar Alterações",
+                enabled = title.value.isNotBlank() && description.value.isNotBlank() && duration.value.isNotBlank(),
+            )
 
-                Spacer(modifier = Modifier.height(8.dp))
-
-//                if (false) {
-//                    SecondButton(
-//                        onClick = { /* Usar modelo */ },
-//                        modifier = Modifier.fillMaxWidth(),
-//                        text = "Usar Modelo"
-//                    )
-//
-//                    Spacer(modifier = Modifier.height(8.dp))
-//
-//                    Button(
-//                        onClick = { /* Excluir modelo */ },
-//                        modifier = Modifier.fillMaxWidth(),
-//                        shape = RoundedCornerShape(50),
-//                        colors = ButtonDefaults.buttonColors(
-//                            containerColor = Color.Transparent,
-//                            contentColor = Palette.primaryColor
-//                        )
-//                    ) {
-//                        Text("Excluir Modelo")
-//                    }
-//                }
-            }
+            Spacer(modifier = Modifier.height(16.dp))
         }
-
     }
 }
 
