17c9e1ba46bf74efb491f30265174e16512c2d7f
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-08 03:48:28 -0300

    feat: add Reminder use cases and ViewModels to Modules

--- app/src/main/java/com/silva021/minhajornada/di/Modules.kt
+++ app/src/main/java/com/silva021/minhajornada/di/Modules.kt
@@ -9,6 +9,12 @@ import com.silva021.minhajornada.data.repository.CommunitiesRepository
 import com.silva021.minhajornada.data.repository.CommunitiesRepositoryImpl
 import com.silva021.minhajornada.data.repository.ProfileRepository
 import com.silva021.minhajornada.data.repository.ProfileRepositoryImpl
+import com.silva021.minhajornada.data.repository.ReminderRepository
+import com.silva021.minhajornada.data.repository.ReminderRepositoryImpl
+import com.silva021.minhajornada.domain.usecases.CreateChallengeUseCase
+import com.silva021.minhajornada.domain.usecases.CreateProfileUseCase
+import com.silva021.minhajornada.domain.usecases.CreateReminderUseCase
+import com.silva021.minhajornada.domain.usecases.DeleteReminderUseCase
 import com.silva021.minhajornada.domain.usecases.GetChallengeByIdUseCase
 import com.silva021.minhajornada.domain.usecases.GetChallengesUseCase
 import com.silva021.minhajornada.domain.usecases.GetCommentsByPostIdUseCase
@@ -16,17 +22,25 @@ import com.silva021.minhajornada.domain.usecases.GetCommunitiesByCategoryUseCase
 import com.silva021.minhajornada.domain.usecases.GetCommunitiesUseCase
 import com.silva021.minhajornada.domain.usecases.GetCommunityByIdUseCase
 import com.silva021.minhajornada.domain.usecases.GetCommunityPostByIdUseCase
-import com.silva021.minhajornada.domain.usecases.GetMyProfileUseCase
 import com.silva021.minhajornada.domain.usecases.GetCommunityPostsUseCase
+import com.silva021.minhajornada.domain.usecases.GetMyProfileUseCase
 import com.silva021.minhajornada.domain.usecases.GetPublicChallengesByCategoryUseCase
 import com.silva021.minhajornada.domain.usecases.GetPublicChallengesUseCase
+import com.silva021.minhajornada.domain.usecases.GetReminderByIdUseCase
+import com.silva021.minhajornada.domain.usecases.LoginUseCase
+import com.silva021.minhajornada.domain.usecases.UpdateReminderUseCase
+import com.silva021.minhajornada.ui.screens.challenges.actives.ActiveChallengesViewModel
+import com.silva021.minhajornada.ui.screens.challenges.create.CreateChallengeViewModel
 import com.silva021.minhajornada.ui.screens.challenges.mine.ChallengesViewModel
 import com.silva021.minhajornada.ui.screens.challenges.reminders.RemindersViewModel
+import com.silva021.minhajornada.ui.screens.challenges.reminders.create.CreateReminderViewModel
 import com.silva021.minhajornada.ui.screens.communities.details.CommunityDetailsViewModel
 import com.silva021.minhajornada.ui.screens.communities.feed.CommunityFeedViewModel
 import com.silva021.minhajornada.ui.screens.communities.list.CommunitiesViewModel
 import com.silva021.minhajornada.ui.screens.communities.post.CommunityPostViewModel
 import com.silva021.minhajornada.ui.screens.explorer.list.ExplorerViewModel
+import com.silva021.minhajornada.ui.screens.login.LoginViewModel
+import com.silva021.minhajornada.ui.screens.login.signup.SignUpViewModel
 import com.silva021.minhajornada.ui.screens.profile.ProfileViewModel
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -39,18 +53,32 @@ val viewModelModule = module {
     viewModel { ChallengesViewModel(get()) }
     viewModel { CommunitiesViewModel(get(), get()) }
     viewModel { ProfileViewModel(get()) }
-    viewModel { ExplorerViewModel(get(), get( )) }
+    viewModel { ExplorerViewModel(get(), get()) }
     viewModel { CommunityDetailsViewModel(get()) }
     viewModel { CommunityFeedViewModel(get(), get(), get()) }
     viewModel { CommunityPostViewModel(get(), get(), get()) }
-    viewModel { RemindersViewModel(get()) }
+    viewModel { RemindersViewModel(get(), get()) }
+    viewModel { CreateReminderViewModel(get(), get(), get()) }
+    viewModel { CreateChallengeViewModel(get()) }
+    viewModel { ActiveChallengesViewModel(get()) }
+
+    viewModel { SignUpViewModel(get()) }
+    viewModel { LoginViewModel(get()) }
 }
 
 val usecasesModule = module {
+    factory { LoginUseCase() }
+
     factory { GetChallengesUseCase(get()) }
     factory { GetChallengeByIdUseCase(get()) }
+    factory { CreateChallengeUseCase(get()) }
+    factory { CreateReminderUseCase(get()) }
+    factory { DeleteReminderUseCase(get()) }
+    factory { GetReminderByIdUseCase(get()) }
+    factory { UpdateReminderUseCase(get()) }
 
     factory { GetMyProfileUseCase(get()) }
+    factory { CreateProfileUseCase(get()) }
 
     factory { GetCommunitiesUseCase(get()) }
     factory { GetCommunitiesByCategoryUseCase(get()) }
@@ -88,5 +116,6 @@ val repositoryModule = module {
     single<ChallengeRepository> { ChallengesRepositoryImpl(get()) }
     single<ProfileRepository> { ProfileRepositoryImpl(get()) }
     single<CommunitiesRepository> { CommunitiesRepositoryImpl(get()) }
+    single<ReminderRepository> { ReminderRepositoryImpl() }
 
 }
