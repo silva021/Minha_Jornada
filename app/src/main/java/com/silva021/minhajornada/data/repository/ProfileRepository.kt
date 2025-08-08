ad5d69aeacf3e2342350ee0a5332671b487e124b
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-08 03:42:24 -0300

    feat: add createProfile method to ProfileRepository for saving user profiles

--- app/src/main/java/com/silva021/minhajornada/data/repository/ProfileRepository.kt
+++ app/src/main/java/com/silva021/minhajornada/data/repository/ProfileRepository.kt
@@ -1,17 +1,27 @@
 package com.silva021.minhajornada.data.repository
 
 import com.silva021.minhajornada.data.api.ProfileApi
+import com.silva021.minhajornada.data.datastore.FireStoreHelper
 import com.silva021.minhajornada.data.dto.ProfileDTO
 import com.silva021.minhajornada.ui.profilesDTO
+import kotlinx.coroutines.tasks.await
+import kotlin.text.set
 
 class ProfileRepositoryImpl(
     private val profileApi: ProfileApi
 ) : ProfileRepository {
+    val usersCollection = FireStoreHelper.usersCollection
+
     override suspend fun getProfile(): ProfileDTO {
         return profilesDTO.first()
+    }
+
+    override suspend fun createProfile(profile: ProfileDTO) {
+        usersCollection.document(profile.id).set(profile).await()
     }
 }
 
 interface ProfileRepository {
     suspend fun getProfile(): ProfileDTO
+    suspend fun createProfile(profile: ProfileDTO)
 }
