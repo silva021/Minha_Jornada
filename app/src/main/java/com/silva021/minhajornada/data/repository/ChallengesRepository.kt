d5445e435eab5eb28f9c1b2ae867bfa7fe3dc0b1
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-08 03:42:48 -0300

    feat: implement ChallengesRepository to manage user challenges with Firestore

--- app/src/main/java/com/silva021/minhajornada/data/repository/ChallengesRepository.kt
+++ app/src/main/java/com/silva021/minhajornada/data/repository/ChallengesRepository.kt
@@ -1,20 +1,46 @@
 package com.silva021.minhajornada.data.repository
 
-import com.silva021.minhajornada.data.api.ChallengesApi
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.silva021.minhajornada.data.datastore.FireStoreHelper
 import com.silva021.minhajornada.data.dto.ChallengeDTO
 import com.silva021.minhajornada.data.dto.PublicChallengeDTO
 import com.silva021.minhajornada.ui.DatabaseFake
+import kotlinx.coroutines.tasks.await
 
 class ChallengesRepositoryImpl(
-    val challengesApi: ChallengesApi,
+    private val reminderRepository: ReminderRepository,
 ) : ChallengeRepository {
+    val userChallengesCollection by lazy { FireStoreHelper.userChallengesCollection }
     override suspend fun getChallenges(): List<ChallengeDTO> {
-//        return challengesApi.getChallenges()
-        return DatabaseFake.challengesDto
+        val challenges = userChallengesCollection
+            .get()
+            .await()
+            .documents.mapNotNull { document ->
+                document.toObject(ChallengeDTO::class.java)
+            }
+
+        return challenges
     }
 
     override suspend fun getPublicChallenges(): List<PublicChallengeDTO> {
         return DatabaseFake.publicChallenges
+    }
+
+    override suspend fun createChallenge(challenge: ChallengeDTO) {
+        Firebase.auth.currentUser?.uid ?: throw Exception("Usuário não autenticado")
+        userChallengesCollection.document(challenge.id).set(challenge).await()
+    }
+
+    override suspend fun updateChallengeProgress(
+        challengeId: String,
+        progress: Int,
+    ): ChallengeDTO {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteChallenge(challengeId: String) {
+        TODO("Not yet implemented")
     }
 
 }
@@ -22,8 +48,8 @@ class ChallengesRepositoryImpl(
 interface ChallengeRepository {
     suspend fun getChallenges(): List<ChallengeDTO>
     suspend fun getPublicChallenges(): List<PublicChallengeDTO>
-//    suspend fun createChallenge(challenge: ChallengeDTO): ChallengeDTO
-//    suspend fun updateChallengeProgress(challengeId: String, progress: Int): ChallengeDTO
-//    suspend fun deleteChallenge(challengeId: String)
+    suspend fun createChallenge(challenge: ChallengeDTO)
+    suspend fun updateChallengeProgress(challengeId: String, progress: Int): ChallengeDTO
+    suspend fun deleteChallenge(challengeId: String)
 
 }
