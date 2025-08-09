6796a2e604774b1e30fa3ced7b7ac450d0b1d9de
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-09 18:54:53 -0300

    feat: implement navigation event for successful login in LoginScreen

--- app/src/main/java/com/silva021/minhajornada/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/silva021/minhajornada/ui/screens/login/LoginScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import com.silva021.minhajornada.ui.screens.defaults.loading.LoadingScreen
+import com.silva021.minhajornada.ui.screens.login.signup.NavigationEvent
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -14,17 +15,20 @@ fun LoginScreen(
 ) {
     val uiState = viewModel.uiState.collectAsState()
 
+    LaunchedEffect(Unit) {
+        viewModel.eventFlow.collect {
+            when(it) {
+                NavigationEvent.NavigateToChallengeScreen -> {
+                    onLoginSuccess.invoke()
+                }
+            }
+        }
+    }
+
     when (val state = uiState.value) {
         is LoginScreenState.Loading -> {
             LoadingScreen()
         }
-
-        is LoginScreenState.Success -> {
-            LaunchedEffect(Unit) {
-                onLoginSuccess.invoke()
-            }
-        }
-
         is LoginScreenState.Idle, is LoginScreenState.Error -> {
             val errorMessage = if (state is LoginScreenState.Error) state.message else null
             LoginContent(
--- app/src/main/java/com/silva021/minhajornada/ui/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/silva021/minhajornada/ui/screens/login/LoginViewModel.kt
@@ -3,8 +3,13 @@ package com.silva021.minhajornada.ui.screens.login
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.silva021.minhajornada.domain.usecases.LoginUseCase
+import com.silva021.minhajornada.ui.screens.login.signup.NavigationEvent
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
@@ -15,6 +20,11 @@ class LoginViewModel(
     private val _uiState = MutableStateFlow<LoginScreenState>(LoginScreenState.Idle)
     val uiState: StateFlow<LoginScreenState> = _uiState.asStateFlow()
 
+    private val _eventFlow = MutableSharedFlow<NavigationEvent>(
+        extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    val eventFlow: Flow<NavigationEvent> = _eventFlow.asSharedFlow()
+
     fun login(email: String, password: String) {
         viewModelScope.launch {
             _uiState.value = LoginScreenState.Loading
@@ -23,7 +33,7 @@ class LoginViewModel(
                 email = email,
                 password = password
             ).onSuccess {
-                _uiState.value = LoginScreenState.Success
+                _eventFlow.emit(NavigationEvent.NavigateToChallengeScreen)
             }.onFailure { messageError ->
                 _uiState.value = LoginScreenState.Error(messageError.message ?: "Não identificamos o erro, entre em contato com o suporte")
             }
@@ -33,7 +43,6 @@ class LoginViewModel(
 
 sealed class LoginScreenState {
     object Idle : LoginScreenState()
-    object Success : LoginScreenState()
     object Loading : LoginScreenState()
     data class Error(val message: String) : LoginScreenState()
 }
