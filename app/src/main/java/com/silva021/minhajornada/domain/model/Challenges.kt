b8df1391a1fa3b813665600876aada6d43656799
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-08-08 22:37:36 -0300

    refactor: remove unused imports and ChallengeDTO extension function from Challenges.kt

--- app/src/main/java/com/silva021/minhajornada/domain/model/Challenges.kt
+++ app/src/main/java/com/silva021/minhajornada/domain/model/Challenges.kt
@@ -1,23 +1,6 @@
 package com.silva021.minhajornada.domain.model
 
-import com.silva021.minhajornada.data.dto.ChallengeDTO
-import com.silva021.minhajornada.data.dto.CheckInDTO
-import com.silva021.minhajornada.data.dto.ReminderDTO
-
 data class Challenges(
     val actives: List<Challenge>,
     val completed: List<Challenge>
 )
-
-fun ChallengeDTO.toDomain(): Challenge {
-    return Challenge(
-        id = this.id,
-        title = this.title,
-        description = this.description,
-        startDate = this.startDate,
-        checkins = this.checkins.map { it.toDomain() },
-        categoryType = CategoryType.valueOf(this.categoryType.uppercase()),
-        durationType = DurationType.valueOf(this.durationType.uppercase()),
-        reminders = this.reminders.map { it.toDomain() },
-    )
-}
